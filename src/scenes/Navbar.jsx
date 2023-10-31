5b2c274cb5a7b99cbe8bbf109645669f90fdc20e
FumCode <fumcode@gmail.com>
2023-10-31 20:22:40 +0200

    add /yarn add framer-motion react-anchor-link-smooth-scroll/ dep.

---
+++ src/scenes/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState } from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+const Navbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
