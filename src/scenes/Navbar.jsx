f49a2b61c074e141cf63991961a33ce65303039b
FumCode <fumcode@gmail.com>
2023-11-01 11:36:35 +0200

    fix bug

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useState } from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import useMediaQuery from '../hooks/useMediaQuery';
@@ -20,7 +19,7 @@ const Link = ({ page, selectedPage, setSelectedPage}) => {
 
 const Navbar = ({selectedPage, setSelectedPage}) => {
     const [isMenuToggled, setIsMenuToggled] = useState(false);
-    const isAboveMediumScreens = useMediaQuery('(min-width: 768px)');
+    const isAboveSmallScreens = useMediaQuery('(min-width: 768px)');
 
   return (
     <nav className={'z-40 w-full fixed top-0 py-6'}>
