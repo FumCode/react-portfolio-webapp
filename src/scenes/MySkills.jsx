5bed9292fe8fcabd0005b06f9ed462014cb0a2c7
FumCode <fumcode@gmail.com>
2023-11-04 14:42:02 +0200

    small fixes

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -72,9 +72,9 @@ const MySkills = () => {
                         <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
                     </div>
                     <p className="mt-5">
-                        A auctor pharetra hendrerit mattis amet etiam interdum platea. Est
-                        morbi porttitor scelerisque fermentum, sagittis non egestas. Amet
-                        odio sit sagittis,
+                        Lorem Ipsum is simply dummy text of the printing and typesetting
+                        industry. Lorem Ipsum has been the industry's standard dummy text
+                        ever since the 1500s
                     </p>
                 </motion.div>
 
@@ -100,10 +100,9 @@ const MySkills = () => {
                         <div className="w-1/2 md:w-3/4 h-32 bg-red absolute right-0 top-0 z-[-1]" />
                     </div>
                     <p className="mt-5">
-                        Urna, eget pulvinar dolor cursus volutpat dictum odio. Nec ultricies
-                        amet in in neque nibh tortor. Libero sed pretium justo nulla blandit
-                        nulla amet habitant iaculis. Iaculis in congue vitae sollicitudin
-                        faucibus a
+                        Lorem Ipsum is simply dummy text of the printing and typesetting
+                        industry. Lorem Ipsum has been the industry's standard dummy text
+                        ever since the 1500s
                     </p>
                 </motion.div>
                 {/* IMAGINATIVE */}
@@ -128,10 +127,9 @@ const MySkills = () => {
                         <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]" />
                     </div>
                     <p className="mt-5">
-                        Accumsan eu fringilla nisi, eget. Vitae, eget ut id proin arcu in
-                        curabitur. Lectus libero, egestas enim aliquam quis felis amet.
-                        Sagittis, amet netus fringilla netus lobortis odio sed platea.
-                        Bibendum.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting
+                        industry. Lorem Ipsum has been the industry's standard dummy text
+                        ever since the 1500s
                     </p>
                 </motion.div>
             </div>
