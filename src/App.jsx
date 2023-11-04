e6f220a0a4ca893989bc2ea35a39d132cdb07395
FumCode <fumcode@gmail.com>
2023-11-04 14:48:16 +0200

    add Projects

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Landing from "./scenes/Landing";
 import DotGroup from "./scenes/DotGroup";
 import MySkills from "./scenes/MySkills";
 import LineGradient from "./components/LineGradient";
-// import Projects from "./scenes/Projects";
+import Projects from "./scenes/Projects";
 // import Contact from "./scenes/Contact";
 // import Footer from "./scenes/Footer";
 import useMediaQuery from "./hooks/useMediaQuery";
@@ -61,15 +61,15 @@ function App() {
                 </motion.div>
             </div>
             <LineGradient />
-            {/*<div className="w-5/6 mx-auto">*/}
+            <div className="w-5/6 mx-auto">
             {/*    <motion.div*/}
             {/*        margin="0 0 -200px 0"*/}
             {/*        amount="all"*/}
             {/*        onViewportEnter={() => setSelectedPage("projects")}*/}
             {/*    >*/}
-            {/*        <Projects />*/}
+                    <Projects />
             {/*    </motion.div>*/}
-            {/*</div>*/}
+            </div>
             {/*<LineGradient />*/}
             {/*<div className="w-5/6 mx-auto md:h-full">*/}
             {/*    <motion.div*/}
